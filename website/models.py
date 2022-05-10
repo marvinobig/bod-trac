e0883da99d297d07d9d660ff8dec96d5408d52f8
Marvin obig <obig.marv@gmail.com>
2022-05-10 14:37:41 +0100

    insert db models

--- website/models.py
+++ website/models.py
@@ -1,1 +1,21 @@
+from datetime import timezone
+from pickle import TRUE
+from . import db
+from flask_login import UserMixin 
+from sqlalchemy.sql import func
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150))
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150))
+    startingBw = db.Column(db.Numeric)
+    recordedBws = db.relationship('BodyWeight')
+
+class BodyWeight(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    currentDate = db.Column(db.DateTime(timezone=True), default=func.now())
+    bW = db.Column(db.Numeric)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
