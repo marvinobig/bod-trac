09b3df102b7ede57049c22df253815abee4ccadf
Marvin obig <obig.marv@gmail.com>
2022-05-11 14:46:17 +0100

    insert fetch function

--- website/static/script/index.js
+++ website/static/script/index.js
@@ -1,6 +1,17 @@
 const validationAlert = document.getElementById("validation-alert");
 if (validationAlert) {
-  validationAlert.addEventListener("mouseover", () => {
-    validationAlert.remove();
+  setTimeout(removeAlert, 4000);
+}
+
+function removeAlert() {
+  validationAlert.remove();
+}
+
+function deleteWeight(id) {
+  fetch("/deleteWeight", {
+    method: "POST",
+    body: JSON.stringify({ id: id }),
+  }).then((_res) => {
+    window.location.href = "/auth/account";
   });
 }
