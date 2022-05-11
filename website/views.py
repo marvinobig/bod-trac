4336811967f836eee4dd45db0939928382f1bbf0
Marvin obig <obig.marv@gmail.com>
2022-05-11 14:46:01 +0100

    insert add & update & delete routes

--- website/views.py
+++ website/views.py
@@ -1,10 +1,52 @@
-from flask import Blueprint, render_template
+from crypt import methods
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import current_user
+from .models import BodyWeight, User
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', currentUser=current_user)
 
 
+@views.route('/add', methods=['POST'])
+def add_weight():
+    currentWeight = request.form.get('currentWeight')
+    if currentWeight:
+        newBodyweight = BodyWeight(bW=currentWeight, user_id=current_user.id)
+        db.session.add(newBodyweight)
+        db.session.commit()
+        flash('Weight updated', category='success')
+        return render_template('account.html', currentUser=current_user)
+    else:
+        flash('No weight entered', category='error')
+        return render_template('account.html', currentUser=current_user)
+
+
+@views.route('/deleteWeight', methods=['POST'])
+def delete_weight():
+    chosenWeight = json.loads(request.data)
+    weight = chosenWeight['id']
+    getWeight = BodyWeight.query.get(weight)
+    if getWeight:
+        if getWeight.user_id == current_user.id:
+            db.session.delete(getWeight)
+            db.session.commit()
+    return jsonify({})
+
+
+@views.route('/updateStartingBw', methods=['POST'])
+def update_starting_weight():
+    newstartingBw = request.form.get('startingBw')
+
+    if newstartingBw:
+        currentStartingBw = User.query.filter(User.id == current_user.id).one()
+        currentStartingBw.startingBw = newstartingBw
+        db.session.commit()
+    flash('Updated starting bodyweight', category='success')
+    return render_template('account.html', currentUser=current_user)
+    
